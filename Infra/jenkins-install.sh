ce78433113be41373edc2392015d2088e4be90fa
DimiCT <dimitri.griparis@ctengineeringgroup.com>
2024-08-30 11:40:17 +0300

    Update jenkins-install.sh
    add ownership to the infra folder for the user Jenkins to have sudo on all workspaces

--- Infra/jenkins-install.sh
+++ Infra/jenkins-install.sh
@@ -70,3 +70,7 @@ sudo apt install unzip
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
+
+# Ensure Jenkins has the proper ownership and permissions on the workspace directory
+sudo chown -R jenkins:jenkins /var/lib/jenkins/workspace
+sudo chmod -R 755 /var/lib/jenkins/workspace
